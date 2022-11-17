e88b391646a940abdfd95d41fb3606aaf75b543c
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-17 04:09:02 +0000

    Function that takes a string and returns and object with the key being a letter and the value being an array of the indexes where the letter was used

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+function eqArrays(arr1, arr2) {
+  // loop through arr1
+  for (let i = 0; i < arr1.length; i++) {
+    // loop through arr2
+    for (let x = 0; x < arr2.length; x++) {
+      // compare current value of arr1 to current value arr2
+      if (arr1[i] === arr2[x]) {
+        // if values are the same return true
+        return true;
+        // else return false
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+function assertArraysEqual(boolean) {
+  if (boolean === true) {
+    console.log("These arrays are the same!");
+  } else {
+    console.log("These arrays are not the same!");
+  }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// test
+console.log(letterPositions("lighthouse in the house"));
+console.log(letterPositions("hello"));
+assertArraysEqual(eqArrays(letterPositions("hello").e, [1]));
